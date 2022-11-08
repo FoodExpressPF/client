b7833f80003f9234d51b34f8666f28888ac2f995
aguss_5ok <leandroagustinhurtado@gmail.com>
2022-11-08 16:28:35 -0300

    start with redux and done all folders structures

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+// falta importar switch y router 
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        
+          <h1>Food Express</h1>
+        
+      </div>
+    </BrowserRouter>
+  );
+
+}
+
+export default App;
