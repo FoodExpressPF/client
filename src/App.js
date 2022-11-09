73c53b3f67d095009c230c70b274cc11cd220d57
Silviaksm <sksalazarm@gmail.com>
2022-11-09 14:16:30 -0600

    delete duplicate imports

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import LandingPage from './Components/LandingPage'
 import Home from './Components/Home';
-<<<<<<< HEAD
+
 import ProductsForm from './Components/ProductsForm'
 import Detail from './Components/Detail';
-=======
-import ProductsForm from './Components/ProductsForm';
-import Detail from './Components/Detail';
 
->>>>>>> 623078df2ba859f6774ae957bdc279ca6f6373d5
+// import ProductsForm from './Components/ProductsForm';
+// import Detail from './Components/Detail';
+
+// >>>>>>> 623078df2ba859f6774ae957bdc279ca6f6373d5
 
 
 function App() {
