012163de58c61989a893511850056ba5a3af0a56
aguss_5ok <leandroagustinhurtado@gmail.com>
2022-11-10 20:21:41 -0300

    solucionado la navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import LandingPage from './Components/LandingPage/LandingPage'
-import Home from './Components/Home';
+import { Route } from 'react-router-dom';
 
+import LandingPage from './Components/LandingPage/LandingPage';
+import Home from './Components/Home';
 import ProductsForm from './Components/ProductsForm'
 import Detail from './Components/Detail';
 import NavBar from './Components/NavBar';
 
 
 function App() {
-  return (
-    <BrowserRouter>
-     <NavBar />
-      <Routes>
-        <Route path='/' element={<LandingPage />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/createProduct' element={<ProductsForm />} />
-        <Route path='/foods/:id' element={<Detail/>} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <>
+    <Route path='/' exact element={<LandingPage />} />
+    <Route path='/home'>
+      <NavBar />
+      <Home />
+    </Route>
+    <Route path='/createProduct'>
+      <NavBar />
+      <ProductsForm />
+    </Route>
+    <Route path='/foods/:id'>
+      <NavBar />
+      <Detail />
+    </Route>
+  </>
 }
 
 export default App;
