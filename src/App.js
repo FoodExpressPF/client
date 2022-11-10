9ca5309861f7a1f1e08423a2c5232570a8cfc73d
Silviaksm <sksalazarm@gmail.com>
2022-11-10 00:47:16 -0600

    add images folder and Landing styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import LandingPage from './Components/LandingPage'
+import LandingPage from './Components/LandingPage/LandingPage'
 import Home from './Components/Home';
 
 import ProductsForm from './Components/ProductsForm'
---
+++ src/Components/LandingPage/LandingPage.jsx
@@ -0,0 +1,38 @@
+
+import React from 'react'
+import { Link } from 'react-router-dom'
+import style from "./LandingPage.module.css";
+
+
+
+export default function LandingPage() {
+    return (
+
+
+        <div className={style.divInit}>
+            <div className="p">
+                <img className={style.img} src="https://res.cloudinary.com/dowhfu3fj/image/upload/v1668061068/recipes/Dise%C3%B1o_sin_t%C3%ADtulo_7_ia4jsg.png" alt="not found" whidth="200px" height="150px" />
+
+            </div>
+            <div >
+                <Link to='/home'>
+                    <button className={style.botonA}>Home</button>
+                </Link>
+            </div>
+            {/* <div >
+                <Link to='/home'>
+                    <button className={style.botonB}>Login</button>
+                </Link>
+            </div>
+             */}
+            <div>
+
+                <h1 className={style.texto}>Reserve Online</h1>
+
+            </div>
+
+        </div>
+
+    )
+}
+
