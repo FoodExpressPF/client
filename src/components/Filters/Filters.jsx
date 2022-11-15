b400bbc2da7084045f0fa5459cbccca159052ded
aguss_5ok <leandroagustinhurtado@gmail.com>
2022-11-14 21:22:57 -0300

    Gran cambio en la organizacion de las carpetas, mejora en redux, cambio de todas las importaciones, eliminados los tests, mejorados los Tabs de todos los archivos, y cambios menores en general

---
+++ src/components/Filters/Filters.jsx
@@ -0,0 +1,108 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { TYPES_FOODS, SWITCH_HANDLER } from '../../utils/constants.js';
+import { INITIAL_FILTERS } from '../../utils/initialObjects.js';
+import { filterQueryParser } from '../../utils/parsers.js';
+import { getByFilters } from '../../redux/actions.js';
+
+import './Filters.css';
+
+function Filters({ menu }) {
+  const dispatch = useDispatch();
+  const [ filters, setFilters ] = useState(INITIAL_FILTERS);
+
+  const onChangeHandler = (e) => setFilters({...filters, [e.target.name]: e.target.value});
+  const onToggleHandler = (e) => setFilters({...filters, [e.target.name]: !filters[e.target.name]});
+  const onSwitchHandler = (e) => setFilters({...filters, [e.target.name]: SWITCH_HANDLER[filters[e.target.name]]});
+  
+  const onResetHandler = () => setFilters(INITIAL_FILTERS);
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    dispatch(getByFilters(filterQueryParser(filters)));
+  }
+
+  return (
+    <form className={`menu-container ${menu && "open"}`} onSubmit={onSubmitHandler}>
+      <div className="input-group justify-content-center">
+        <div className="form-outline w-50">
+          <input
+            type="search"
+            name="name"
+            className="form-control"
+            onChange={onChangeHandler}
+            placeholder="Search for a plate..."
+            value={filters.name}
+          />
+          <label className="form-label" htmlFor="name"></label>
+        </div>
+      </div>
+      <label htmlFor="type">
+        Type
+      </label>
+      <select
+        name="type"
+        className="slct"
+        onChange={onChangeHandler}
+        value={filters.type}
+      >
+        {TYPES_FOODS.map((type, i) =>
+          <option value={type} key={i}>
+            {type}
+          </option>
+        )}
+      </select>
+
+      <button
+      className='button1'
+        name="asc"
+        type="button"
+        onClick={onToggleHandler}
+      >
+        {/* cambiar por ícono */}
+        {filters.asc ? 'asc' : 'desc'}
+      </button>
+
+      <button
+      className='button1'
+        name="sortby"
+        onClick={onSwitchHandler}
+      >
+        {/* cambiar por ícono */}
+        {filters.sortby}
+      </button>
+
+      <label className="form-check-label" htmlFor="offer">
+        OFFERT!!
+      </label>
+      <button
+        name="offer"
+        type="button"
+        onClick={onSwitchHandler}
+      >
+        {/* cambiar por ícono */}
+        {filters.offer}
+      </button>
+        
+      <button
+        type="submit"
+        className="btn btn-primary h-50"
+        onClick={onSubmitHandler}
+      >
+        <i className="fas fa-search">Go</i>
+      </button>
+
+      <button
+        type="button"
+        className="btn btn-primary h-50"
+        onClick={onResetHandler}
+      >
+        {/* cambiar por ícono */}
+        Reset
+      </button>
+    </form>
+  )
+}
+
+export default Filters;
--- src/utils/checkRoles.js
+++ src/utils/checkRoles.js
@@ -1,17 +1,16 @@
 import { useAuth0 } from "@auth0/auth0-react";
 
 function useCheckRoles(role) {
+  const roleClaimType = 'http://expressfood.net/roles'
+  const { user, isAuthenticated } = useAuth0();
+  const roles = isAuthenticated?user[roleClaimType]:[];
+  const isIncluded = roles.includes(role);
 
-    const roleClaimType = 'http://expressfood.net/roles'
-    const { user, isAuthenticated } = useAuth0();
-    const roles = isAuthenticated?user[roleClaimType]:[];
-    const isIncluded = roles.includes(role);
-
-    if (isIncluded) {
-      return true
-    } else {
-        return false
-    }
+  if (isIncluded) {
+    return true;
+  } else {
+    return false;
   }
+}
 
-  export default useCheckRoles
+export default useCheckRoles;
