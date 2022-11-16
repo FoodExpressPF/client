5a5de94929db0983213c92b203ae8a2b7315639c
javito96 <javier.cornejo.niederle@gmail.com>
2022-11-16 20:00:18 -0300

    config para deploy

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ import store from "./redux/store.js";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 import App from "./App.js";
+import dotenv from 'dotenv'
+dotenv.config();
+
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
 ReactDOM.render(
   <Provider store={store}>
