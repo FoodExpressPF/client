2f1f573a9236419b32a487bcbfa7649de8cecf81
Julio <jncohenb@gmail.com>
2022-11-21 12:33:38 -0300

    estilo a payment aprobado y rechazado + card fix

--- src/pages/postBuy/denegated.jsx
+++ src/pages/postBuy/denegated.jsx
@@ -5,6 +5,7 @@ import { useHistory } from "react-router-dom";
 import useLocalStorage from "../../hooks/useLocalStorage";
 import { sendEmail } from "../../redux/actions";
 import { PaymentDeclined } from "../../emails/emailsDefault.jsx";
+import s from "../postBuy/passed.module.css"
 
 function Denegated() {
   const Cart = useLocalStorage("CART", "");
@@ -23,8 +24,17 @@ function Denegated() {
   };
   return (
     <>
-      <h1>Pago rechazado</h1>
-      <button onClick={() => email()}>Regresar al home</button>
+    <div>
+      <div>
+        <img src="https://res.cloudinary.com/dbepwtmru/image/upload/v1669043972/Pngtree_wrong_and_cancle_symbol_8742801_h75suz.png"
+        className={s.error_image}
+        />      
+        <h1 class="text-center text-danger">Payment declined</h1>
+        <p class="text-center">We are unable to process your online payment from the account provided.
+          Please update it and try again later.</p>
+        <button className={s.denegated_button} onClick={() => email()}>Back to home</button>
+      </div>
+    </div>
     </>
   );
 }
--- src/pages/postBuy/passed.jsx
+++ src/pages/postBuy/passed.jsx
@@ -5,12 +5,15 @@ import { useHistory } from "react-router-dom";
 import useLocalStorage from "../../hooks/useLocalStorage";
 import { sendEmail } from "../../redux/actions";
 import { PaymentConfirmed } from "../../emails/emailsDefault.jsx";
+import s from "../postBuy/passed.module.css"
 
 function Passed() {
   const Cart = useLocalStorage("CART", "");
   const { user } = useAuth0();
   const dispatch = useDispatch();
   const history = useHistory();
+
+
 
   const email = () => {
     let price = Cart.items.reduce((acum, act) => {
@@ -22,10 +25,37 @@ function Passed() {
     Cart.reset();
     history.push("/");
   };
+
   return (
     <>
-      <h1>Pago aprobado</h1>
-      <button onClick={() => email()}>Regresar al home</button>
+    <div className={s.background}>
+      <div className={s.recipt_container}>
+        <img
+          src="https://res.cloudinary.com/dbepwtmru/image/upload/v1669035810/Pngtree_green_checkmark_on_shield_tick_6392392_ktkudu.png"
+          className={s.green_check}
+        />
+        <div>
+          <h1 class="text-center text-success">Payment successful</h1>
+          <div class="d-flex justify-content-between p-2">
+            <div><p>Nombre:</p></div>
+            <div><p>Julio Cohen{/*{user.name} {user.family_name*/}</p></div>
+          </div>
+          <div class="d-flex justify-content-between p-2">
+            <div><p>Email:</p></div>
+            <div><p>jncohenb@gmail.com{/*{user.email}*/}</p></div>
+          </div>
+          <div class="d-flex justify-content-between p-3">
+            <div><p class="fw-bold">Amount paid:</p></div>
+            <div><p class="fw-bold">500.00{/*{price}*/}</p></div>
+          </div>
+          <div class="d-flex justify-content-between p-2">
+            <div><p>Transaction id:</p></div>
+            <div><p>89578937413098{/*{id?}*/}</p></div>
+          </div>
+          <button className={s.home_button} onClick={() => email()}>Back to home</button>
+        </div>
+      </div>
+    </div>
     </>
   );
 }
