d1a48a4b32ac0450adb68dd56d453dd0254ac8f9
Silviaksm <sksalazarm@gmail.com>
2022-11-30 01:04:27 -0600

    change css variable name in landing

--- src/pages/Landing/About/About.jsx
+++ src/pages/Landing/About/About.jsx
@@ -10,12 +10,12 @@ function About(){
     <img src="https://res.cloudinary.com/dowhfu3fj/image/upload/v1668575898/recipes/Dise%C3%B1o_sin_t%C3%ADtulo_8_cifgue.png" alt="" className="imageAbout"/>
     <div>
     <h2>About</h2>
-    <p className='{style.aboutR}'><FaQuoteLeft className='quote'/>
+    <p className='{style.aboutR}'><FaQuoteLeft className='quoteUno'/>
     The restaurant that takes the best of international gastronomy,
     the fusion of these flavors, techniques, ingredients and
     experiences allow us to offer the best dishes to our customers,
     was founded in 2018 and continues to be the best option to taste
-    any recipe international <FaQuoteRight className='quote'/>
+    any recipe international <FaQuoteRight className='quoteUno'/>
     </p>
     <a href="#sec3">
     <button className='aboutButton'>Explore More</button>
