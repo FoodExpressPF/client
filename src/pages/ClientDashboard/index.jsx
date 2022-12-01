d87b9439e0a743f11c6b3111d4ed0a8412b95038
aguss_5ok <leandroagustinhurtado@gmail.com>
2022-12-01 13:14:19 -0300

    agregada seccion de favoritos en user

--- src/pages/ClientDashboard/index.jsx
+++ src/pages/ClientDashboard/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import './index.css';
 
 //components
@@ -8,6 +7,7 @@ import useLocalStorage from '../../hooks/useLocalStorage';
 import ClientOrdersTab from './ClientOrdersTab';
 import NavBar from '../../components/NavBar/NavBar';
 import ClientBookingTab from './ClientBookingTab';
+import FavoritesList from "./FavoritesList.jsx";
 
 export default function ClientDashboard(){
     const Cart = useLocalStorage("CART", "");
@@ -19,29 +19,35 @@ export default function ClientDashboard(){
             <div className='userDashConteiner'>
                 <h2>Welcome!</h2>
             </div>
-            <ul class="nav nav-tabs" id="myTab" role="tablist">                                                 
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link active" id="user-info-tab" data-bs-toggle="tab" data-bs-target="#user-info-tab-pane" type="button" role="tab" aria-controls="user-info-tab-pane" aria-selected="true">User Info</button>
+            <ul className="nav nav-tabs" id="myTab" role="tablist">                                                 
+                <li className="nav-item" role="presentation">
+                    <button className="nav-link active" id="user-info-tab" data-bs-toggle="tab" data-bs-target="#user-info-tab-pane" type="button" role="tab" aria-controls="user-info-tab-pane" aria-selected="true">User Info</button>
                 </li>
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link" id="orders-tab" data-bs-toggle="tab" data-bs-target="#orders-tab-pane" type="button" role="tab" aria-controls="orders-tab-pane" aria-selected="false">Orders</button>
+                <li className="nav-item" role="presentation">
+                    <button className="nav-link" id="orders-tab" data-bs-toggle="tab" data-bs-target="#orders-tab-pane" type="button" role="tab" aria-controls="orders-tab-pane" aria-selected="false">Orders</button>
                 </li>
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link" id="booking-tab" data-bs-toggle="tab" data-bs-target="#booking-tab-pane" type="button" role="tab" aria-controls="booking-tab-pane" aria-selected="false">Booking</button>
+                <li className="nav-item" role="presentation">
+                    <button className="nav-link" id="booking-tab" data-bs-toggle="tab" data-bs-target="#booking-tab-pane" type="button" role="tab" aria-controls="booking-tab-pane" aria-selected="false">Booking</button>
+                </li>
+                <li className="nav-item" role="presentation">
+                    <button className="nav-link" id="favorites-list" data-bs-toggle="tab" data-bs-target="#favorites-list-pane" type="button" role="tab" aria-controls="favorites-list-pane" aria-selected="false">Favorites</button>
                 </li>
                 
             </ul>
                 <div class="tab-content" id="myTabContent">
                     <div className='ContDash'>
-                    <div class="tab-pane fade show active infoTable" id="user-info-tab-pane" role="tabpanel" aria-labelledby="user-info-tab" tabindex="0">
+                    <div className="tab-pane fade show active infoTable" id="user-info-tab-pane" role="tabpanel" aria-labelledby="user-info-tab" tabindex="0">
                         <ClientTabInfo/>
                     </div>
                     </div>
-                    <div class="tab-pane fade" id="orders-tab-pane" role="tabpanel" aria-labelledby="orders-tab" tabindex="0">
+                    <div className="tab-pane fade" id="orders-tab-pane" role="tabpanel" aria-labelledby="orders-tab" tabindex="0">
                         <ClientOrdersTab/>
                     </div>
-                    <div class="tab-pane fade" id="booking-tab-pane" role="tabpanel" aria-labelledby="booking-tab" tabindex="0">
+                    <div className="tab-pane fade" id="booking-tab-pane" role="tabpanel" aria-labelledby="booking-tab" tabindex="0">
                         <ClientBookingTab/>
+                    </div>
+                    <div className="tab-pane fade" id="favorites-list-pane" role="tabpanel" aria-labelledby="favorites-list" tabindex="0">
+                        <FavoritesList/>
                     </div>
                 </div>
                 
